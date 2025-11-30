67fa5d8cf2b2790800e623321d9af3e7a832157f
Takeshi Kishi <takeyaqa@gmail.com>
2025-11-30 04:31:01 +0000

    ci: ESLint/Prettier setup
    - Adjusted `tsconfig.json` formatting for consistency.
    - Refactored imports in `wasm-src/index.ts` for clarity and added type error handling.
    - Created `.prettierignore` to exclude specific files from formatting.
    - Added `.prettierrc` configuration for consistent code style.
    - Introduced `eslint.config.js` for linting JavaScript and TypeScript files with Prettier integration.

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  eslintConfigPrettier,
+])
