618d052ed939a860684942f9eb87252d651edf28
Takeshi Kishi <takeyaqa@gmail.com>
2026-01-12 12:34:04 +0900

    chore: update .prettierignore and eslint.config.js (#43)

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,16 @@
 import js from '@eslint/js'
-import eslintConfigPrettier from 'eslint-config-prettier/flat'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['.emsdk', 'dist']),
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
+    plugins: { js, tseslint },
+    extends: ['js/recommended', 'tseslint/strict', 'tseslint/stylistic'],
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
   },
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
   eslintConfigPrettier,
 ])
