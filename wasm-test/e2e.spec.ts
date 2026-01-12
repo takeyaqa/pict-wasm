43663b8fe1deeb1b1a771416fe42936fc9252bc3
Takeshi Kishi <takeyaqa@gmail.com>
2026-01-12 01:29:20 +0000

    test: add additional parameters to PictRunner.run error test

--- wasm-test/e2e.spec.ts
+++ wasm-test/e2e.spec.ts
@@ -14,6 +14,14 @@ describe('PictRunner', () => {
       expect(() => {
         pictRunner.run([
           { name: 'Type', values: 'Single, Span, Stripe, Mirror, RAID-5' },
+          { name: 'Size', values: '10, 100, 500, 1000, 5000, 10000, 40000' },
+          { name: 'Format method', values: 'Quick, Slow' },
+          { name: 'File system', values: 'FAT, FAT32, NTFS' },
+          {
+            name: 'Cluster size',
+            values: '512, 1024, 2048, 4096, 8192, 16384, 32768, 65536',
+          },
+          { name: 'Compression', values: 'ON, OFF' },
         ])
       }).toThrowError('PictRunner not initialized')
     })
